1fcef85db7c714cd0121ce6f45ecbdacb33bacb6
Batyrhan21 <baatyr3030@gmail.com>
2021-12-16 17:45:55 +0600

    commit3

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.db.models.deletion import PROTECT
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -5,19 +6,43 @@ from main.models import Cinema, Movie
 from rest_framework.response import Response
 from .serializers import MovieSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])  
 def cinema_list_view(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(data=serializer.data)
+    elif request.method == 'POST':
+        title = request.data['title']
+        description = request.data['description', '']
+        cinema = request.data['cinema']
+        genres = request.data['genres', []]
+        movie = Movie.objects.create(
+            title=title, description=description, cinema=cinema,
+        )
+        movie.save()
+        movie.genres.set(genres)
+        movie.save()
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def cinema_detail_view(request, id):
     try:
         movie = Movie.objects.get(id=id)
     except Movie.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND,
                         data={'eror':'Movie not found'})
-    serializer = MovieSerializer(movie, many=False)
-    return Response(data=serializer.data)
+    if request.method == 'GET':
+        serializer = MovieSerializer(movie, many=False)
+        return Response(data=serializer.data)
+    elif request.method == 'PUT':
+        movie.title == request.data['title']
+        movie.description == request.data['description']
+        movie.cinema == request.data['cinema']
+        movie.genres.set(request.data['genres'])
+        movie.save()
+        return Response(data={'massage': 'Movie updated!'})
+    else:
+        movie.delete()
+        return Response()
+
 
