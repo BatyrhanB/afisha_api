14b0303f6372daae3be844dece5a8c7ec244df72
Batyrhan21 <baatyr3030@gmail.com>
2022-01-06 16:33:16 +0600

    prod_config

--- afisha_api/settings.py
+++ afisha_api/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9trs0sw1l7cuemxochq6qf+a*%ijl4_rcg7!$jajve@d875xgn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
